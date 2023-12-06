f933dbb1ce72d49856e4226f92480c8a49020f4d
jane <janehyun03@gmail.com>
2023-12-05 20:03:28 -0500

    props practices

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,109 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Heading from "./component/Heading.jsx";
+
+function Movie(props) {
+  return (
+    <>
+      <span>{props.name}</span>
+      <span>{props.hasWatched && "has Watched"}</span>
+    </>
+  );
+}
+
+function ContactTable(props) {
+  return (
+    <>
+      <table>
+        <tr>
+          {props.name && (
+            <>
+              <td>{props.name}</td>
+            </>
+          )}
+        </tr>
+        <tr>
+          {props.companyName && (
+            <>
+              <td>{props.companyName}</td>
+            </>
+          )}
+        </tr>
+        <tr>
+          {props.category ? (
+            <>
+              <td>{props.category}</td>
+            </>
+          ) : (
+            <>
+              <td>contact could not list category</td>
+            </>
+          )}
+        </tr>
+      </table>
+    </>
+  );
+}
+
+function Weekday(props) {
+  return (
+    <>
+      <div style={{ width: "200px" }}>{props.weekday}</div>
+      <div style={{ width: "200px" }}>{props.day}</div>
+    </>
+  );
+}
 
 function App() {
-  let developerName = "Jane";
+  let customer1ContactForm = {
+    name: "John",
+    companyName: "NIKE",
+    category: "",
+  };
+
+  let customer2ContactForm = {
+    name: "Kevin",
+    companyName: "Adidas",
+    category: "Business",
+  };
+
+  let arr1 = [customer1ContactForm, customer2ContactForm];
+
+  const weekday = [
+    "Monday",
+    "Tuseday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let arr = [];
+  let daysToShow = 12;
+  for (let i = 0; i < daysToShow; i++) {
+    arr.push({ weekday: weekday[i % weekday.length], day: i + 1 });
+    {
+      console.log(arr);
+    }
+  }
 
   return (
     <>
-      <h1>Hello React!</h1>
-      <p>12/04/2023 08:05PM</p>
-      <Heading />
-      <p>written by {developerName}</p>
+      <Movie name="How Grinch Stole Christmas" hasWatched={false} />
+      <br></br>
+      <Movie name="Harry Potter" hasWatched={true} />
+      <br></br>
+      <ContactTable
+        name={customer1ContactForm.name}
+        companyName={customer1ContactForm.companyName}
+        category={customer1ContactForm.category}
+      />
+      <ContactTable {...customer2ContactForm} />
+      {arr1.map((customer) => (
+        <ContactTable {...customer} />
+      ))}
+
+      {arr.map((week, i) => (
+        <Weekday key={i} {...week} />
+      ))}
     </>
   );
 }
